150ae947703f19622f921ae858b4a9bce98315c1
Влад <rtlistate@gmail.com>
2020-01-17 11:42:23 +0300

    Исправляет прыгание фиксированного фона в ie11 при прокрутке страницы

---
+++ source/js/ie-fix-bg-bug.js
@@ -0,0 +1,16 @@
+'use strict';
+
+(function () {
+  var fixIeBackground = function () {
+    if (navigator.userAgent.match(/Trident\/7\./)) {
+      document.body.addEventListener('mousewheel', function () {
+        event.preventDefault();
+        var wd = event.wheelDelta;
+        var csp = window.pageYOffset;
+        window.scrollTo(0, csp - wd);
+      });
+    }
+  };
+
+  fixIeBackground();
+})();
